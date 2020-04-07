51cc7186b8587f214f42fc6c8e13a35b9fec6827
alexandrehalas <alexandrehalas@gmail.com>
2020-04-07 18:47:44 -0300

    tunning project - created core-module and shared-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
 import { CategoriesModule } from './pages/categories/categories.module';
 import { EntriesModule } from './pages/entries/entries.module';
-
-import { HttpClientModule } from '@angular/common/http';
-
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataBase } from './in-memory-database';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
+    CoreModule,
     AppRoutingModule,
     CategoriesModule,
     EntriesModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   providers: [],
   bootstrap: [AppComponent]
