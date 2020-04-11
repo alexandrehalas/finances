045e8af2688245206db229b01e67eb919278d9d7
Alexandre Halas <alexandrehalas@gmail.com>
2020-04-10 22:15:31 -0300

    Created module and component reports. Improved lazy-load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'categories', loadChildren: () => import ('./pages/categories/categories.module').then(module => module.CategoriesModule) },
   { path: 'entries', loadChildren: () => import ('./pages/entries/entries.module').then(module => module.EntriesModule) },
-  { path: '', redirectTo: '', pathMatch: 'full' }
+  { path: '', loadChildren: () => import ('./pages/reports/reports.module').then(module => module.ReportsModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
-import { CategoriesModule } from './pages/categories/categories.module';
-import { EntriesModule } from './pages/entries/entries.module';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     CoreModule,
-    AppRoutingModule,
-    CategoriesModule,
-    EntriesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
